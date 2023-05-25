d7771c4f430998afcd548eda16b78406fe1c2cf5
Saadraees007 <127970153+Saadraees007@users.noreply.github.com>
2023-05-26 04:15:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var display = document.getElementById ("display");
+
+
+function getValue (num) {
+    display.value += num;
+}
+function calculateValue () {
+    var ans = eval(display.value);
+    display.value = ans
+}
+function clearAll () {
+    display.value = ""
+}
